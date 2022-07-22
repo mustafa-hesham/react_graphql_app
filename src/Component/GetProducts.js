7e42fc25289f2198b1655f534ae77f52602db5ee
Mustafa Hesham <mustafa.hesham@scandiweb.com>
2022-07-22 21:58:53 +0200

    Removed callback function from the api url link

--- src/Component/GetProducts.js
+++ src/Component/GetProducts.js
@@ -1,12 +1,10 @@
 import React, { Component } from "react";
 import axios from "axios";
 
-let searchCriteria = "hund";
+let searchCriteria = "vitamin c";
 let limit = 20;
 
-const url = 'https://api.clerk.io/v2/search/search?payload=%7B%22offset%22%3A0%2C%22limit%22%3A+'+limit+'+%2C%22query%22%3A%22+'+searchCriteria+'+3%22%2C%22attributes%22%3A%5B%22entity_id%22%2C%22name%22%2C%22description%22%2C%22price%22%2C%22list_price%22%2C%22image%22%2C%22url%22%2C%22categories%22%2C%22brand%22%2C%22sku%22%2C%22age%22%2C%22on_sale%22%2C%22product_type%22%2C%22product_type%22%2C%22short_description%22%2C%22menge%22%2C%22bestseller%22%2C%22news_from_date%22%2C%22news_to_date%22%2C%22review_count%22%2C%22rating_summary%22%2C%22url_key%22%2C%22id%22%2C%22product_type%22%2C%22stock%22%2C%22list_price%22%2C%22short_description%22%2C%22menge%22%5D%2C%22facets%22%3A%5B%22categories%22%5D%2C%22key%22%3A%22rNjIR5qdAeP8chB6Z3O2OAimA447bcWY%22%2C%22visitor%22%3A%22auto%22%2C%22_%22%3A1658479443%7D&callback=__clerk_cb_7';
-const regex = /(<([^>]+)>)/ig;
-
+const url = 'https://api.clerk.io/v2/search/search?payload=%7B%22offset%22%3A0%2C%22limit%22%3A+'+limit+'+%2C%22query%22%3A%22+'+searchCriteria+'+3%22%2C%22attributes%22%3A%5B%22entity_id%22%2C%22name%22%2C%22description%22%2C%22price%22%2C%22list_price%22%2C%22image%22%2C%22url%22%2C%22categories%22%2C%22brand%22%2C%22sku%22%2C%22age%22%2C%22on_sale%22%2C%22product_type%22%2C%22product_type%22%2C%22short_description%22%2C%22menge%22%2C%22bestseller%22%2C%22news_from_date%22%2C%22news_to_date%22%2C%22review_count%22%2C%22rating_summary%22%2C%22url_key%22%2C%22id%22%2C%22product_type%22%2C%22stock%22%2C%22list_price%22%2C%22short_description%22%2C%22menge%22%5D%2C%22facets%22%3A%5B%22categories%22%5D%2C%22key%22%3A%22rNjIR5qdAeP8chB6Z3O2OAimA447bcWY%22%2C%22visitor%22%3A%22auto%22%2C%22_%22%3A1658479443%7D';
 
 export default class Product extends Component{
     constructor(props){
@@ -18,11 +16,9 @@ export default class Product extends Component{
     }
     componentDidMount(){
         axios.get(url).then((response) => {
-            let result = response['data'].replace(regex, '');
-            result = result.substring(13);
-            result = result.slice(0, -2);
-            result = JSON.parse(result);
-            this.setState({content:result["product_data"]})
+            console.log(response['data']['product_data']);
+            let result = response['data']['product_data'];
+            this.setState({content:result})
             console.log(this.state.content);
         });
     }
@@ -32,8 +28,11 @@ export default class Product extends Component{
                 <div>
                     <h3>Serach criteria: </h3><h2>{ searchCriteria }</h2>
                     { this.state.content && this.state.content.map((val) => {
-                        return <h1>{ val["name"] } - { val["sku"] }</h1>;
-                    })}
+                        return (
+                        <div>
+                            <h1 key={ val["sku"] }>{ val["name"] } - { val["sku"] }</h1>
+                        </div>
+                    )})}
                 </div>
             )
         }
